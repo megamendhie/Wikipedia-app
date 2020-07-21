cc28cd8a5d8fb7bf0414ddb218dc3448338213db
Mendhie <imendhie@yahoo.com>
2020-07-21 08:08:22 +0100

    updated gradle files

--- app/src/main/java/com/sqube/wikipedia/WikiApplication.kt
+++ app/src/main/java/com/sqube/wikipedia/WikiApplication.kt
@@ -1,4 +1,27 @@
-package repositories
+package com.sqube.wikipedia
 
-class WikiApplication {
+import android.app.Application
+import com.sqube.wikipedia.managers.WikiManager
+import com.sqube.wikipedia.providers.ArticleDataProvider
+import com.sqube.wikipedia.repositories.ArticleDatabaseOpenhelper
+import com.sqube.wikipedia.repositories.FavouritesRepo
+import com.sqube.wikipedia.repositories.HistoryRepo
+
+class WikiApplication: Application() {
+    private var dbHelper: ArticleDatabaseOpenhelper? = null
+    private var favouritesRepo: FavouritesRepo? = null
+    private var historyRepo: HistoryRepo? = null
+    private var wikiProvider: ArticleDataProvider? = null
+    var wikiManager: WikiManager? = null
+            private set
+
+    override fun onCreate() {
+        super.onCreate()
+
+        dbHelper = ArticleDatabaseOpenhelper(applicationContext)
+        favouritesRepo = FavouritesRepo(dbHelper!!)
+        historyRepo = HistoryRepo(dbHelper!!)
+        wikiProvider = ArticleDataProvider()
+        wikiManager = WikiManager(wikiProvider!!, favouritesRepo!!, historyRepo!!)
+    }
 }
